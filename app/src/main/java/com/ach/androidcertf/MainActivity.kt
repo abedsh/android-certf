4ebfa168b8d662e356f73c97cf66d630c5358f87
Abed shmaytilli <abed.chmaitelli@gmail.com>
2019-07-03 09:57:29 -0400

    Initial commit

---
+++ app/src/main/java/com/ach/androidcertf/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.ach.androidcertf
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) { // the ? is to allow a null saveInstanceState https://kotlinlang.org/docs/reference/null-safety.html
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+
+
+
+
+
+}
